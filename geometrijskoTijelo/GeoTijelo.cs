77aedd4397e9a764dd7d2b15fa203000b64d0a8c
Matej <Matej@MLAPTOP>
2022-03-26 23:30:45 +0100

    Add project files.

---
+++ geometrijskoTijelo/GeoTijelo.cs
@@ -0,0 +1,32 @@
+﻿using geometrijskoTijelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometrijskoTijelo
+{
+    internal class GeoTijelo
+    {
+        double Volumen, Oplosje;
+        public static int brojac = 0;
+
+        public GeoTijelo()
+        {
+            brojac++;
+        }
+    }
+
+    class Kugla : GeoTijelo
+    {
+    }
+
+    class Kvadar : GeoTijelo
+    {
+    }
+
+    class Kocka : Kvadar
+    {
+    }
+}
---
+++ geometrijskoTijelo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometrijskoTijelo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GeoTijelo.brojac);
+            Kugla x = new Kugla();
+            Console.WriteLine(GeoTijelo.brojac);
+            Kvadar y = new Kvadar();
+            Console.WriteLine(GeoTijelo.brojac);
+            Kocka z = new Kocka();
+            Console.WriteLine(GeoTijelo.brojac);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
